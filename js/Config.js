1029ad0b5379abce178cb20291164ad1302022af
FNCxPro <relative@airmail.cc>
2018-07-05 08:47:36 -0400

    Add config file and have secrets.wundergroundAPIKey
    This commit allows for easier configuration in  one config file...
    instead of searching around different files for simple fields

---
+++ js/Config.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+  crawl: `Thanks to all the contributors of this project. While it's not completely finished, the community effort has made this possible. Stars, contributions, and feedback are welcome and appreciated. Thanks for trying out this emulator.`,
+  secrets: {
+    wundergroundAPIKey: 'd8585d80376a429e'
+  }
+}
