9715f0e5ea859467d60b98fc6dec6a34b870adae
iRickey - Mrqualbal <95340847+Mattharan@users.noreply.github.com>
2022-12-20 12:26:47 +0700

    Update MainScript.js

--- js/MainScript.js
+++ js/MainScript.js
@@ -92,7 +92,7 @@ function setInformation(){
 }
 
 function setMainBackground(){
-  getElement('background-image').style.backgroundImage = 'url(https://imgur.com/gallery/CivwWzz';
+  getElement('background-image').style.backgroundImage = 'url(https://i.imgur.com/kbcB9As.jpeg';
 }
 
 function checkStormMusic(){
